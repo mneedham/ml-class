863aef81ad60a961179b65d06d42dae672328c38
Mark Needham <m.h.needham@gmail.com>
2011-11-02 09:00:11 +0000

    mark: current state of logistical regression exercises@

--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -17,10 +17,17 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+predictions = sigmoid(X * theta);
+penalty0 = -y' * log(predictions);
+penalty1 = (1-y)' * log(1 - predictions);
 
+J = 1/m * sum(penalty0 - penalty1) + lambda/(2*m) * sum(theta(2:length(theta)) .* theta(2:length(theta)));
 
+grad(1) = 1/m * sum((predictions - y) .* X(:, 1));
 
-
+for i=2:size(theta)
+  grad(i) = 1/m * sum(((predictions - y) .* X(:, i)) + (lambda * theta)(i));
+end
 
 % =============================================================
 
--- mlclass-ex2/predict.m
+++ mlclass-ex2/predict.m
@@ -15,10 +15,17 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+for i=1:m
+  prediction = sigmoid(X(i, :) * theta);
+  if(prediction >= 0.5)
+    p(i) = 1;
+  else
+    p(i) = 0;
+  endif
+end
 
 
-
-
+% if >= 0.5 then it becomes 1 else 0
 
 
 % =========================================================================
